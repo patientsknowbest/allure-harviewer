43d694b50475f40abbdd63c665b184d76d7aebd2
Eduardo Fernandes <efernandes@MacBook-Pro-3.home>
2018-09-10 15:11:03 +0100

    Update how attachments are passed to the viewer depending on test status

--- static/index.js
+++ static/index.js
@@ -4,30 +4,30 @@
     events: {
       'click .harviewer__link': 'viewHar',
     },
-    viewHar: function(event) {
+    viewHar: function (event) {
       var x = window.open('', '', 'location=no,toolbar=no,scrollbars=yes');
-      x.document.write('<html><head>'+
-        '<link rel="stylesheet" href="https://gitgrimbo.github.io/harviewer/master/css/harViewer.css" type="text/css">'+
-        '<script src="https://gitgrimbo.github.io/harviewer/master/scripts/jquery.js"></script>'+
-        '</head>'+
-        '<body>'+
-        '  <div id="content"></div>'+
-        '  <script data-main="https://gitgrimbo.github.io/harviewer/master/scripts/harViewer.js" src="https://gitgrimbo.github.io/harviewer/master/scripts/require.js"></script>'+
-        '  <script>'+
-        '  $("#content").bind("onViewerPreInit", function(event) {'+
-        '    var viewer = event.target.repObject;'+
-        '    viewer.removeTab("Home");'+
-        '    viewer.removeTab("About");'+
-        '    viewer.removeTab("Schema");'+
-        '    viewer.removeTab("HAR");'+
-        '    viewer.showTabBar(false);'+
-        '  }).bind("onViewerInit", function(event) {'+
-        '    var viewer = event.target.repObject;'+
-        '    var file = location.hash.substr(1);'+
-        '    viewer.loadHar("'+jQuery(event.currentTarget || event.target).data('har')+'");'+
-        '  });'+
-        '  </script>'+
-        '</body>'+
+      x.document.write('<html><head>' +
+        '<link rel="stylesheet" href="https://gitgrimbo.github.io/harviewer/master/css/harViewer.css" type="text/css">' +
+        '<script src="https://gitgrimbo.github.io/harviewer/master/scripts/jquery.js"></script>' +
+        '</head>' +
+        '<body>' +
+        '  <div id="content"></div>' +
+        '  <script data-main="https://gitgrimbo.github.io/harviewer/master/scripts/harViewer.js" src="https://gitgrimbo.github.io/harviewer/master/scripts/require.js"></script>' +
+        '  <script>' +
+        '  $("#content").bind("onViewerPreInit", function(event) {' +
+        '    var viewer = event.target.repObject;' +
+        '    viewer.removeTab("Home");' +
+        '    viewer.removeTab("About");' +
+        '    viewer.removeTab("Schema");' +
+        '    viewer.removeTab("HAR");' +
+        '    viewer.showTabBar(false);' +
+        '  }).bind("onViewerInit", function(event) {' +
+        '    var viewer = event.target.repObject;' +
+        '    var file = location.hash.substr(1);' +
+        '    viewer.loadHar("' + jQuery(event.currentTarget || event.target).data('har') + '");' +
+        '  });' +
+        '  </script>' +
+        '</body>' +
         '</html>');
     },
     template: function (data) {
@@ -35,22 +35,27 @@
         return;
       }
       var links = [];
-      for (var i in data.testStage.attachments) {
-        var attach = data.testStage.attachments[i];
+      var attachments = data.status === "passed" ? 
+        data.testStage.attachments : 
+        data.testStage.steps.find(function (e) { 
+          return e.name === "Collect debug information on failure" 
+        }).attachments;
+      for (var i in attachments) {
+        var attach = attachments[i];
         if (attach.name.match(/\.har$/)) {
-          links.push({url: 'data/attachments/'+attach.source, name: attach.name});
+          links.push({ url: 'data/attachments/' + attach.source, name: attach.name });
         }
       }
       if (links.length) {
         var ret = '<h3 class="pane__section-title">Inspect requests</h3>' +
             '<div class="harviewer__content">';
         for (var i in links) {
-          ret += '<div class="harviewer__link step__title step__title_hasContent long-line" '+
-              'data-har="'+links[i].url+'">'+
-            '<span class="step__arrow block__arrow">'+
-            '<span class="fa fa-chevron-right fa-fw text_status_passed"></span></span>'+
-            '<div class="step__name">'+
-            links[i].name+
+          ret += '<div class="harviewer__link step__title step__title_hasContent long-line" ' +
+              'data-har="' + links[i].url + '">' +
+            '<span class="step__arrow block__arrow">' +
+            '<span class="fa fa-chevron-right fa-fw text_status_passed"></span></span>' +
+            '<div class="step__name">' +
+            links[i].name +
             '</div>';
         }
         ret += '</div></div>';
@@ -58,5 +63,5 @@
       }
     }
   });
-  (allure.api.addTestcaseBlock || allure.api.addTestResultBlock).call(allure.api, harViewer, {position: 'before'});
+  (allure.api.addTestcaseBlock || allure.api.addTestResultBlock).call(allure.api, harViewer, { position: 'before' });
 })();
